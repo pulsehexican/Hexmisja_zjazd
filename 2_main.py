30622c3ea2abadca9d93cbc3b44059361bbaf940
pulsehexican <123429695+pulsehexican@users.noreply.github.com>
2023-03-14 03:12:40 +0100

    Add files via upload

---
+++ 2_main.py
@@ -0,0 +1,14 @@
+# --------------------------funkcje
+def add(number1, number2):
+    result = number1 + number2
+    return (result)
+
+
+# --------------------kod program
+print('hello world')
+num1 = 555
+num2 = 5555
+
+sum_result = add(num1, num2)
+
+print(sum_result)
---
+++ 3_typy_danych.py
@@ -0,0 +1,18 @@
+
+zmienna_integer =100
+zmienna_float =3.69
+zmienna_string ='HEX'
+zmienna_string2 = '100.1'
+zmienna_bool_prawda =True
+zmienna_bool_falsz =False
+
+print(zmienna_integer)
+print(zmienna_float)
+print(zmienna_string)
+print(zmienna_bool_prawda)
+print(zmienna_bool_falsz)
+
+rzutowanie = float(zmienna_string2)+0.50
+
+print(rzutowanie)
+
